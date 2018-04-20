2896c77bea88841657e24d888afef06df95e8eeb
DeepCFoxXx <learncode23@gmail.com>
2018-04-20 12:45:29 +0100

    .map added for songs

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -5,6 +5,24 @@ const Chart = (props) => {
   if (props.songs == null || props.songs.length === 0) {
     return <p>Loading...</p>;
   }
+  return (
+    <div>
+      {props.songs.map((song, index) => {
+        return (
+          <Song
+            key={index}
+            position={index + 1}
+            title={song['im:name'].label}
+            artist={song['im:artist'].label}
+            image={song['im:image'][1].label}
+            audio={song.link[1].attributes.href}
+            handlePlayPause={props.handlePlayPause}
+          />
+        )
+      })}
+    </div>
+  );
+};
 }
 
 export default Chart;
