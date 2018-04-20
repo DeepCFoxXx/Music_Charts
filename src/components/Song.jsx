591c2563f62ace92e077df34849f9e56e84bb9ed
DeepCFoxXx <learncode23@gmail.com>
2018-04-20 13:38:30 +0100

    play button added

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -3,8 +3,22 @@ import React, {Component} from 'react';
 class Song extends Component {
 
   render(){
+    const altTag = `${this.props.title} by ${this.props.artist}`;
 
+    return (
+      <div className="song">
+        <img
+          alt={"Play " + altTag}
+          id={this.props.position}
+          className='audio-control'
+          src='https://www.mariowiki.com/images/thumb/f/f5/StarMK8.png/200px-StarMK8.png'
+          onClick={() => {this.props.handlePlayPause(this.audio)}}
+        />
+
+      </div>
+    );
   }
 };
+
 
 export default Song;
